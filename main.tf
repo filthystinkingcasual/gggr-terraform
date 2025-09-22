d4395a8a5eb22638047555f1df5c200c02930750
Mara W <37845942+filthystinkingcasual@users.noreply.github.com>
2025-09-22 12:26:17 -0400

    we did things

--- main.tf
+++ main.tf
@@ -39,11 +39,18 @@ data "aws_iam_policy_document" "gha_ecr_public_role_policy" {
       "ecr-public:InitiateLayerUpload",
       "ecr-public:PutImage",
       "ecr-public:UploadLayerPart",
+<<<<<<< HEAD
       "ecr-public:Describe*",
       "ecr-public:GetAuthorizationToken"
     ]
 
     resources = ["*"]
+=======
+      "ecr-public:Describe*"
+    ]
+
+    resources = [aws_ecrpublic_repository.gogogadget.arn]
+>>>>>>> e79ef42 (we did things)
     effect    = "Allow"
   }
 }
